3b59e38b51f1c50b2b79ebc04c47778d0619a1ec
saksham311 <138859736+saksham311@users.noreply.github.com>
2025-04-10 13:49:25 +0530

    Create program2.cbl

---
+++ program2.cbl
@@ -0,0 +1,28 @@
+               IDENTIFICATION DIVISION.
+       PROGRAM-ID. AVERAGE-CALCULATOR.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 NUM1          PIC 9(4) VALUE 0.
+       01 NUM2          PIC 9(4) VALUE 0.
+       01 NUM3          PIC 9(4) VALUE 0.
+       01 SUM           PIC 9(5) VALUE 0.
+       01 AVERAGE       PIC 9V99 VALUE 0.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Enter the first number: " WITH NO ADVANCING.
+           ACCEPT NUM1.
+           DISPLAY "Enter the second number: " WITH NO ADVANCING.
+           ACCEPT NUM2.
+           DISPLAY "Enter the third number: " WITH NO ADVANCING.
+           ACCEPT NUM3.
+
+           COMPUTE SUM = NUM1 + NUM2 + NUM3.
+           COMPUTE AVERAGE = SUM / 3.
+
+           DISPLAY "The average of the three numbers is: " AVERAGE.
+
+           STOP RUN. 
